06b23fa9a3030f6a9e1b1ac22707b5d1f48ef3b8
Rodolfo Rivas Rodriguez <rodolfo.rivas@catolica.edu.sv>
2025-04-04 11:45:44 -0600

    historial pedido y detalle hecho, falta imagen y modificacion de vista pero esta funcional

---
+++ ProyectoVentas/Models/ItemOrdenViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoVentas.Models
+{
+    public class ItemOrdenViewModel
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Total { get; set; }
+        public string Descripcion { get; set; }  // Agregar la propiedad Descripción
+    }
+}
--- ProyectoVentas/Models/OrdenViewModel.cs
+++ ProyectoVentas/Models/OrdenViewModel.cs
@@ -5,7 +5,8 @@
         public string NumRecibo { get; set; }
         public string Estado { get; set; }
         public DateTime FechaPedido { get; set; }
-        public List<string> Items { get; set; }
+        public List<ItemOrdenViewModel> Items { get; set; }  // Cambiar de List<string> a List<ItemOrdenViewModel>
         public decimal? Total { get; set; } = 0;
+        public int IdPedido { get; set; } = 1; // Agregar esta propiedad
     }
 }
