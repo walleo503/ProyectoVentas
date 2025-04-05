ccfae79dce7fee45a055e9e35e9aac26388ab5ce
Rodolfo Rivas Rodriguez <rodolfo.rivas@catolica.edu.sv>
2025-04-05 11:06:20 -0600

    Historial de pedidos y detalle hecho falta enlazarlo con la vista anterior a la orden

--- ProyectoVentas/Models/platos.cs
+++ ProyectoVentas/Models/platos.cs
@@ -10,6 +10,7 @@ namespace ProyectoVentas.Models
         public string? nombre { get; set; }
         public string? descripcion {  get; set; }
         public decimal precio { get; set; }
+        public string? imagen { get; set; }
         public int? categoria_id { get; set; }
         public int estado { get; set; }
     }
