9735d35a5ba79e9edff3d6ea51f10bfce6dc6410
Alexisbb19 <109247899+Alexisbb19@users.noreply.github.com>
2025-04-06 18:36:13 -0600

    nuevos...

--- ProyectoVentas/Models/restauranteDbContext.cs
+++ ProyectoVentas/Models/restauranteDbContext.cs
@@ -16,6 +16,11 @@
             public DbSet<empleados> empleados { get; set; }
             public DbSet<platos> platos { get; set; }
             public DbSet<combos> combos { get; set; }
+            public DbSet<promociones> promociones { get; set; }
+            public DbSet<platos_combos> platos_combos { get; set; }
+            public DbSet<menu_combo> menu_combo { get; set; }
+            public DbSet<menu_plato> menu_plato { get; set; }
+            public DbSet<combo_promocion> combo_promocion { get; set; }
             public DbSet<Pedido_Online> pedido_Online { get; set; }
             public DbSet<Cliente> Cliente { get; set; }
             public DbSet<Login_Cliente> Login_Clientes { get; set; }
@@ -62,9 +67,20 @@
             modelBuilder.Entity<Cliente>()
             .HasOne(c => c.LoginCliente)
             .WithOne(p => p.Cliente)
-            .HasForeignKey<Login_Cliente>(l => l.Cliente); 
-                   
+            .HasForeignKey<Login_Cliente>(l => l.Cliente);
 
+
+            modelBuilder.Entity<menu_plato>()
+                    .HasOne(mp => mp.menu)
+                    .WithMany() // Puedes crear una lista en el modelo menu si lo deseas
+                    .HasForeignKey(mp => mp.menu_id)
+                    .HasConstraintName("FK_menu_plato_menu");
+
+                modelBuilder.Entity<menu_plato>()
+                    .HasOne(mp => mp.plato)
+                    .WithMany() // Igualmente, puedes crear una lista en platos si lo necesitas
+                    .HasForeignKey(mp => mp.plato_id)
+                    .HasConstraintName("FK_menu_plato_platos");
 
             base.OnModelCreating(modelBuilder);
             }
